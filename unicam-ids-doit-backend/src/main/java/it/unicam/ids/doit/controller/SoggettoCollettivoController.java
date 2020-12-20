c5ff31fff4c6fedb39ae791f81af0f9c087a6cf0
giulio <giulio.quaresima@gmail.com>
2020-12-20 18:28:18 +0100

    Inizio specifica dettagliata di nuovi casi d'uso.

--- unicam-ids-doit-backend/src/main/java/it/unicam/ids/doit/controller/SoggettoCollettivoController.java
+++ unicam-ids-doit-backend/src/main/java/it/unicam/ids/doit/controller/SoggettoCollettivoController.java
@@ -6,13 +6,11 @@ import java.util.List;
 import java.util.SortedSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.unicam.ids.doit.model.SoggettoCollettivo;
